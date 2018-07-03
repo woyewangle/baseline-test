a5195f7cf3314acf7494ea6caabb89afc7368ed6
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-03 14:44:37 +0800

    second test

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result = new Array();
+   var c=collectionB.toString();
+   for(var i=0;i<collectionA.length;i++){
+      if(c.indexOf(collectionA[i].toString())>-1)
+       {
+          result.push(collectionA[i]);
+       }      
+    }
+    return result;
+
 }
